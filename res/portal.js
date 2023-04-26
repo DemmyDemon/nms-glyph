7765909d96f0f1849717a086cf83d8bccd67c829
Fredrik Vold <demmydemon@gmail.com>
2023-04-26 18:59:15 +0200

    Functioning web interface
    There. It works.
    It's not pretty, but it works.

---
+++ res/portal.js
@@ -0,0 +1,56 @@
+const validAddress = /[A-Z0-9]{16}/
+
+document.addEventListener("DOMContentLoaded", (evt)=>{
+    document.querySelectorAll("button.glyph").forEach( (emt) => {
+        emt.addEventListener("click",clickButton)
+    })
+    document.querySelector("button#clear").addEventListener("click", (evt)=>{
+        document.querySelector("#hexaddress").value = ""
+        document.querySelector("#url").value = ""
+        document.querySelector("#address").classList.add("hidden")
+        validate()
+    })
+    document.querySelector("button#load").addEventListener("click", loadAddress)
+})
+
+function clickButton(evt) {
+    console.log("clicked button", evt.target.value)
+    appendSymbol(evt.target.value)
+}
+
+function appendSymbol(symbol) {
+    let hexaddressField = document.querySelector("#hexaddress")
+    let newValue = hexaddressField.value + symbol
+    if (newValue.length > 16) {
+        newValue = newValue.slice(-16)
+    }
+    hexaddressField.value = newValue
+    validate()
+}
+
+function validate() {
+    let value = document.querySelector("#hexaddress").value
+    let loadButton = document.querySelector("button#load")
+    if (value.match(validAddress)){
+        loadButton.classList.remove("nope")
+        loadButton.disabled = false
+        return true
+    }
+    else {
+        loadButton.classList.add("nope")
+        loadButton.disabled = true
+        return false
+    }
+}
+
+function loadAddress() {
+    if (!validate()) {
+        return
+    }
+    let address = document.querySelector("#hexaddress").value
+    let url = host = window.location.protocol + "//" + window.location.host + "/" + address + ".png"
+    document.querySelector("#url").value = url
+    let image = document.querySelector("#address")
+    image.src = url
+    image.classList.remove("hidden")
+}
